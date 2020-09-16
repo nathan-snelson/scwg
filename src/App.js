4ccc117b9b00455bd7892de100f61f9232962c6d
nathan-snelson <nathansnelson97@gmail.com>
2020-09-16 17:52:32 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import scwglogo from './scwglogo.svg';
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import NavBar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar bg="dark" variant="dark">
+          <NavBar.Brand>
+            <img src={scwglogo} alt="SCWG Logo" height="50" width="50" className="d-inline-block align-top"/>
+          </NavBar.Brand>
+
+          <NavBar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link className="link" to={'/'}>Home</Link>
+              <Link className="link" to={'/about'}>About</Link>
+              <Link className="link" to={'/profile'}>Profile</Link>
+              <Link className="link" to={'/contact'}>Contact</Link>
+              <Link className="link" to={'/events'}>Events</Link>
+            </Nav>
+          </NavBar.Collapse>
+        </NavBar>
+        <hr />
+      </div>
+    </Router>
   );
 }
 
